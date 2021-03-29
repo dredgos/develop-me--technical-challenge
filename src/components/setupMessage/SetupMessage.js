d3095d69ef76cb85beab35ccf40187257aca000e
Paul Dredge <dredgos@googlemail.com>
2021-03-29 12:37:24 +0100

    Added setup instructions

---
+++ src/components/setupMessage/SetupMessage.js
@@ -0,0 +1,9 @@
+const SetupMessage = () => {
+    return (
+        <section>
+            <p>The games can only begin when the numbers of players entered is a minimum of 4 and a power of 2 (4, 8, 16, 32 etc...)</p>
+        </section>
+    );
+};
+
+export default SetupMessage;
