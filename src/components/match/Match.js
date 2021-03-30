97e0888cf632fa21303510afd9c6a96278314fe3
Paul Dredge <dredgos@googlemail.com>
2021-03-30 14:10:10 +0100

    Changed lock in button visbility once clicked

--- src/components/match/Match.js
+++ src/components/match/Match.js
@@ -16,7 +16,7 @@ const Match = ({ player1, player2, sendWinner }) => {
         <div>
             <button value={player1} onClick={handleClick1}>{player1}</button>
             <button value={player2} onClick={handleClick2}>{player2}</button>
-            <p>Winner: {winner}</p>
+            <p>Winner: {winner === "" ? "TBC" : winner}</p>
             {confirmed ? null : <button onClick={handleConfirm}>Lock In Winner</button>}
             
         </div>
