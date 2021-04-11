180cf0e79131ed982a844214a8667cf621e04274
Paul Dredge <dredgos@googlemail.com>
2021-04-11 09:18:57 +0100

    Updates

--- src/components/header/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-        <div class="wordart blues">
-            <h1 className="site_header text">Ping Pong Party</h1>
-        </div>
-    );
-};
-
-export default Header;
